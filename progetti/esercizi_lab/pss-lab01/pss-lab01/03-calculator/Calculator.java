c60ba77bab1905729369339a6b9d6ae04e1c36a8
RED-ITA <enricosalvagiani@gmail.com>
2025-05-30 21:14:07 +0200

    modificato il cazzo di file calculater per la fase 2

--- progetti/esercizi_lab/pss-lab01/pss-lab01/03-calculator/Calculator.java
+++ progetti/esercizi_lab/pss-lab01/pss-lab01/03-calculator/Calculator.java
@@ -1,19 +1,36 @@
 class Calculator {
 
+    int nOpDone;
+    double lastRes;
+
+    void build() {
+        this.nOpDone = 0;
+        this.lastRes = 0;
+
+    }
+
     double add(double v1, double v2) {
-        return v1 + v2;
+        this.nOpDone++;
+        this.lastRes = v1 + v2;
+        return this.lastRes;
     }
 
     double sub(double v1, double v2) {
-        return v1 - v2;
+        this.nOpDone++;
+        this.lastRes =  v1 - v2;
+        return this.lastRes;
     }
 
     double mul(double v1, double v2) {
-        return v1 * v2;
+        this.nOpDone++;
+        this.lastRes =  v1 * v2;
+        return this.lastRes;
     }
 
     double div(double v1, double v2) {
-        return v1 / v2;
+        this.nOpDone++;
+        this.lastRes =  v1 / v2;
+        return this.lastRes;
     }
 
 }
