852449876700268f3cba509b813febd8b1c7b6f6
RED-ITA <enricosalvagiani@gmail.com>
2025-05-30 20:56:39 +0200

    finiti i metodi della clsse

--- progetti/esercizi_lab/pss-lab01/pss-lab01/03-calculator/Calculator.java
+++ progetti/esercizi_lab/pss-lab01/pss-lab01/03-calculator/Calculator.java
@@ -1,1 +1,19 @@
-class Calculator
+class Calculator {
+
+    double add(double v1, double v2) {
+        return v1 + v2;
+    }
+
+    double sub(double v1, double v2) {
+        return v1 - v2;
+    }
+
+    double mul(double v1, double v2) {
+        return v1 * v2;
+    }
+
+    double div(double v1, double v2) {
+        return v1 / v2;
+    }
+
+}
