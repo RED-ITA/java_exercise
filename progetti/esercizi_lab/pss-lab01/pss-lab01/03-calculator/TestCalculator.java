9da3d272f9066f08b632dee5309ac47578532977
RED-ITA <enricosalvagiani@gmail.com>
2025-05-30 21:15:57 +0200

    consenga fase 2 tset calculator

--- progetti/esercizi_lab/pss-lab01/pss-lab01/03-calculator/TestCalculator.java
+++ progetti/esercizi_lab/pss-lab01/pss-lab01/03-calculator/TestCalculator.java
@@ -1,5 +1,9 @@
 class TestCalculator {
   public static void main(String[] args) {
+
+    /*
+     * 4. Si verifichi il funzionamento della nuova calcolatrice modificando `QUESTO FILE` in modo che stampi il valore dei due nuovi campi dopo ogni operazione.
+     */
 	  
       Calculator calc = new Calculator();
       System.out.println("1 + 2 =" + calc.add(1, 2));
